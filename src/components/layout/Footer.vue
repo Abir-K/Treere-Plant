a046a82b23be1132b089db3b2d4e2146cbfede8f
Kader Khan <76205675+Abir-K@users.noreply.github.com>
2024-03-16 21:44:09 +0600

    Add files via upload

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="py-16 flex items-center justify-center text-center bg-[#222C1D] text-white md:text-xl font-light">
+        Made with Love by Abir©2023
+    </footer>
+</template>
---
+++ src/components/ui/Button.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <button class="text-center lg:py-3 py-2 px-6 lg:px-14 bg-transparent border border-white rounded-xl lg:text-2xl text-lg font-normal">
+        <slot></slot>
+    </button>
+</template>
